90fb31a9633459b9fcfc32ed05a548505c03e34d
Varada <Varada@gmail.com>
2026-02-04 22:09:12 +0530

    Hello

---
+++ 09-installing-multiple.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+    echo "Please run as root"
+    exit 1
+else
+    echo "Running as root"
+fi
+
+Install(){
+    if [ $(dnf list installed $1) -ne 0 ]; then
+        echo "Installing $1..."
+        dnf install $1
+        if [ $? -ne 0 ]; then
+            echo "$1 installation is failure"
+            exit 1
+        else
+            echo "$1 installation is success" 
+        fi
+    else
+        echo "$1 is already installed..."
+    fi              
+}
+for i in $@; do
+    Install $i
+done
