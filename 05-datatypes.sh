eef291564d023bf8def12eb2a3f6b631f9233b6d
Varada <Varada@gmail.com>
2026-02-04 17:05:09 +0530

    Hello

--- 05-datatypes.sh
+++ 05-datatypes.sh
@@ -5,3 +5,10 @@ NUM2=200
 
 SUM=$($NUM1+$NUM2)
 echo "$SUM"
+
+
+NAMES=("Shanmukha" "Virat "Dhoni")
+echo "All names are : ${NAMES[@]}"
+echo "All names are : ${NAMES[0]}"
+echo "All names are : ${NAMES[1]}"
+echo "All names are : ${NAMES[2]}"
