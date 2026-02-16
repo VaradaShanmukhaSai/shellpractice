6ad1257da2e8952b2df17deeb930948bd2ceecf8
Varada <Varada@gmail.com>
2026-02-16 15:13:18 +0530

    Committed

---
+++ 27-diskutilization.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DISK_FREE=$(df -hT | awk 'NR>1 {print}')
+IP=$(hostname -i)
+MESSAGE=''
+if [[ ! -z $DISK_FREE ]]; then 
+    echo "Executing command df "
+    while IFS= read -r line ; do
+        USAGE=$($line | awk '{print $6}' | cut -d '%' -f1 )
+        if [[ $USAGE -ge 3 ]]; then 
+            PARTITION=$($line | awk '{print $7}')
+            MESSAGE+="High Disk Usage in $PARTITION : $USAGE%"
+        fi
+    done <<<$DISK_FREE
+else
+    echo "No mounts on the disk "
+fi 
+
+
+sh 28-mail.sh "varadashanmukhasai7@gmail.com" "HIGH DISK ALERT ON $IP" "$MESSAGE" "HIGH DISK USAGE" "$IP" "DevOps Team"
---
+++ 28-mail.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TEAM=$1
+SUBJECT=$2
+MESSAGE_BODY=$3
+ALERT_TYPE=$4
+SERVER=$5
+TO_ADDRESS=$6
+
+FINAL_MESSAGE=$(sed -e 's/TEAM/$TEAM/g' -e 's/SUBJECT/$SUBJECT/g' -e 's/MESSAGE/$MESSAGE_BODY/g' -e 's/SERVER/$SERVER/g' template.html)
+
+{
+echo "To: $TO_ADDRESS"
+echo "Subject: $SUBJECT"
+echo "Content-Type: text/html"
+echo ""
+echo "$FINAL_MESSAGE"
+} | msmtp "$TO_ADDRESS"
