68f333979b87516ee3c638881982cab70049fb59
Varada <Varada@gmail.com>
2026-02-16 15:25:16 +0530

    Committed

--- 28-mail.sh
+++ 28-mail.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-TEAM=$1
+TEAM=$6
 SUBJECT=$2
 MESSAGE_BODY=$3
 ALERT_TYPE=$4
 SERVER=$5
-TO_ADDRESS=$6
+TO_ADDRESS=$1
 
 FINAL_MESSAGE=$(sed -e 's/TEAM/$TEAM/g' -e 's/SUBJECT/$SUBJECT/g' -e 's/MESSAGE/$MESSAGE_BODY/g' -e 's/SERVER/$SERVER/g' template.html)
 
