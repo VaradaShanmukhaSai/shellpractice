d6b1f604804724ad0d87fa5d0c9140e86e6c1963
Varada <Varada@gmail.com>
2026-02-16 07:32:49 +0530

    Committed

---
+++ 25-deleteoldlogs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+R=$'\e[31m'
+G=$'\e[32m'
+Y=$'\e[33m'
+N=$'\e[0m'
+
+LOG_FOLDER="/home/ec2-user/old-logs"
+LOG_FILE=$LOG_FOLDER/$(basename $0).log
+
+if [[ -d $LOG_FOLDER ]]; then 
+    echo "$LOG_FOLDER exists"
+    FILES=$(find $LOG_FOLDER -type f -mtime +14)
+    if [[ -z $FILES ]]; then 
+        echo "Files exist in $LOG_FOLDER older than 14 days"
+        while IFS=read -r file; do
+            echo "Removing $file" &>>$LOG_FILE
+            rm -rf $file
+        done <<<{$FILES}
+     else
+        echo "There are no files in $LOG_FOLDER older than 14 days"
+     fi
+else
+    echo "$LOG_FOLDER the directory doesn't exist"
+fi
+
+
