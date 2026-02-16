4aec2f8eda649edd53072498d50fdd7e11178f11
Varada <Varada@gmail.com>
2026-02-16 15:36:08 +0530

    Committed

--- 27-diskutilization.sh
+++ 27-diskutilization.sh
@@ -3,18 +3,16 @@
 
 IP=$(hostname -i)
 MESSAGE=''
-if [[ ! -z $DISK_FREE ]]; then 
-    echo "Executing command df "
-   df -hT | awk 'NR>1{print $6,$7}' |while IFS= read -r usage part ; do
-        USAGE=$(echo "$usage" |cut -d '%' -f1 )
-        if [[ $USAGE -ge 3 ]]; then 
-            PARTITION=$part
-            MESSAGE+="High Disk Usage in $PARTITION : $USAGE% <br>"
-        fi
-    done 
-else
-    echo "No mounts on the disk "
-fi 
+
+echo "Executing command df "
+df -hT | awk 'NR>1{print $6,$7}' |while IFS= read -r usage part ; do
+    USAGE=$(echo "$usage" |cut -d '%' -f1 )
+    if [[ $USAGE -ge 3 ]]; then 
+        PARTITION=$part
+        MESSAGE+="High Disk Usage in $PARTITION : $USAGE% <br>"
+    fi
+done 
+
 
 
 sh 28-mail.sh "varadashanmukhasai7@gmail.com" "HIGH DISK ALERT ON $IP" "$MESSAGE" "HIGH DISK USAGE" "$IP" "DevOps Team"
